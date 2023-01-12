6c1d471835bdb8f57b86b377c873ffb793a5590d
Breno Toledo <breh_toledo@hotmail.com>
2023-01-11 23:10:23 -0300

    Aula 02 - Formulário PET, Admin

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views #Da pasta usuários import os views
 
+
 urlpatterns = [
     path('cadastro/', views.cadastro, name = 'cadastro'),
     path('login/', views.logar, name="login"),
     path('sair/', views.sair, name="sair"),
-]
+    ] 
